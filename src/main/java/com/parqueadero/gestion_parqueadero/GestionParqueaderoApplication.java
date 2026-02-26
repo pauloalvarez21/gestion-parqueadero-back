6dd5b360027810140fc6f0717e02d948774af84d
Paulo Alvarez <pauloalvarez21@gmail.com>
2026-02-26 11:15:29 -0500

    Primer commit

---
+++ src/main/java/com/parqueadero/gestion_parqueadero/GestionParqueaderoApplication.java
@@ -0,0 +1,13 @@
+package com.parqueadero.gestion_parqueadero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionParqueaderoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionParqueaderoApplication.class, args);
+	}
+
+}
