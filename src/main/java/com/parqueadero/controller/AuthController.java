fad864f8828516d39a2fb7148a285144b5fef353
paulo <pauloalvarez21@gmail.com>
2026-03-04 15:53:14 -0500

    feat: eliminar usuario

--- src/main/java/com/parqueadero/controller/AuthController.java
+++ src/main/java/com/parqueadero/controller/AuthController.java
@@ -13,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,5 +56,15 @@ public class AuthController {
         var user = usuarioRepository.findByUsername(request.getUsername()).orElseThrow();
         var jwtToken = jwtService.generateToken(user);
         return ResponseEntity.ok(AuthResponse.builder().token(jwtToken).build());
+    }
+
+    @DeleteMapping("/eliminar/{username}")
+    public ResponseEntity<Void> eliminarUsuario(@PathVariable String username) {
+        var user = usuarioRepository.findByUsername(username);
+        if (user.isPresent()) {
+            usuarioRepository.delete(user.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
